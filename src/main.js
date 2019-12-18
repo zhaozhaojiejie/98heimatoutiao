56574dd473fa9b44e27ffeaf02aaa8584cd703c7
zhaozhaojiejie <2272280759@qq.com>
2019-12-18 17:28:47 +0800

    黑马头条PC-初始化项目-安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import Element from 'element-ui'
-Vue.use(Element)
+// import Element from 'element-ui'
+import ElementUI from 'element-ui'
+// 引入less出去浏览默认样式
+import './style/index.less'
+// 注册了ElementUI全局注册
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
