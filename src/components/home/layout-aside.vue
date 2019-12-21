2d6824dec2829b86595988ce05e9410959c09e05
zhaozhaojiejie <2272280759@qq.com>
2019-12-21 20:28:55 +0800

    黑马头条PC-主页模块-左侧导航菜单

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,59 @@
+<template>
+<div class='layout-aside'>
+    <div class='title'>
+        <img src="../../assets/img/logo_admin.png" alt="">
+    </div>
+  <el-menu style="width:231px;" background-color=" #323745" text-color="#adafb5" active-text-color=" #ffd04b">
+       <el-menu-item >
+           <!-- 1.导航组件 -->
+           首页保存
+       </el-menu-item>
+       <!-- 3.二级组件 -->
+       <el-submenu>
+           <!-- 4.这里很666哈，通过有名插槽做导航头 -->
+           <span slot='title'>内容管理</span>
+           <!-- 5.用匿名插槽做导航隐藏内容 -->
+           <el-menu-item>发布文章</el-menu-item>
+           <el-menu-item>内容列表</el-menu-item>
+           <el-menu-item>评论列表</el-menu-item>
+           <el-menu-item>素材管理</el-menu-item>
+       </el-submenu>
+       <el-submenu>
+
+           <span slot='title'>粉丝管理</span>
+
+           <el-menu-item>图文数据</el-menu-item>
+           <el-menu-item>粉丝画像</el-menu-item>
+           <el-menu-item>粉丝概况</el-menu-item>
+           <el-menu-item>粉丝列表</el-menu-item>
+       </el-submenu>
+       <el-menu-item >
+           <!-- 2.导航组件 -->
+           账户信息
+       </el-menu-item>
+  </el-menu>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.layout-aside {
+    width: 230px;
+    .title {
+        text-align:center;
+        background-color:#2e2f32;
+        padding:10px 0;
+        img {
+            height: 35px;
+        }
+
+    }
+
+}
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,8 +2,10 @@
 <!-- 1.一个外部容器 -->
     <el-container>
         <!-- 2.左右布局 -->
-        <el-aside>左侧</el-aside>
-        <!-- 再放一个container -->
+        <el-aside style="background-color:#353b4e;height:100vh;width:230px;">
+            <layoutAside></layoutAside>
+        </el-aside>
+        <!-- 3.再放一个container -->
         <el-container>
             <el-header>头部</el-header>
             <el-main>主题区</el-main>
@@ -12,7 +14,12 @@
 </template>
 
 <script>
+// 4.引入左边导航的组件
+import layoutAside from '../../components/home/layout-aside'
 export default {
+  components: {
+    layoutAside
+  }
 
 }
 </script>
