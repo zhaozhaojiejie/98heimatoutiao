b9b6c1321abe40b5bac54e0eb13a348ba3bc8a7e
zhaozhaojiejie <2272280759@qq.com>
2019-12-21 19:25:37 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,14 @@
 <template>
-<div>我是主页</div>
-
+<!-- 1.一个外部容器 -->
+    <el-container>
+        <!-- 2.左右布局 -->
+        <el-aside>左侧</el-aside>
+        <!-- 再放一个container -->
+        <el-container>
+            <el-header>头部</el-header>
+            <el-main>主题区</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
