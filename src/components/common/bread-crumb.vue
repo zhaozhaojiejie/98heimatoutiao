ed0334d94af0a6e8d0d48a4f5292fca7491eca6e
zhaozhaojiejie <2272280759@qq.com>
2019-12-27 19:48:17 +0800

    黑马头条PC-评论列表-评论数据加载到页面及状态过滤

--- src/components/common/bread-crumb.vue
+++ src/components/common/bread-crumb.vue
@@ -1,12 +1,11 @@
 <template>
-  <el-breadcrumb>
+  <el-breadcrumb separator='>'>
       <el-breadcrumb-item to='/home'>首页</el-breadcrumb-item>
       <el-breadcrumb-item>
           <!-- 具名插槽 -->
           <slot name='title'></slot>
 
       </el-breadcrumb-item>
-      <el-breadcrumb-item></el-breadcrumb-item>
 
   </el-breadcrumb>
 </template>
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,5 +1,27 @@
 <template>
-<div>评论区</div>
+<!-- 用卡片来布局 -->
+<el-card>
+    <!-- 使用面包屑组件，首先第一个组件就是一个匿名插槽首页/具名插槽title -->
+    <!-- el-card 有两个部分头和body 这个头部是具名插槽，body是匿名插槽 -->
+    <!-- 头部 -->
+    <bread-crumb slot='header' >
+    <template slot='title'>评论列表</template>
+    </bread-crumb>
+    <!-- body部分 -->
+    <el-table>
+        <el-table-column width='600' label='标题'></el-table-column>
+
+        <el-table-column label='评论状态'></el-table-column>
+
+        <el-table-column label='总评论数'></el-table-column>
+
+        <el-table-column label='粉丝评论数'></el-table-column>
+
+        <el-table-column label='操作'></el-table-column>
+
+    </el-table>
+
+</el-card>
 </template>
 
 <script>
